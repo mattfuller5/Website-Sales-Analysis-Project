3872081160ff3de9af4db88cdb813a92ddbd8baf
Matthew Fuller <45941691+mattfuller5@users.noreply.github.com>
2026-07-09 16:58:46 +0100

    Create SQL script for user events sales funnel analysis
    This SQL file defines a sales funnel with stages based on user events over the last 300 days.

---
+++ sql/02_conversion_rates.sql
@@ -0,0 +1,18 @@
+SELECT * FROM `project-1a7f0a69-00af-4bc7-9b9.sql_practice.user_events` LIMIT 1000
+
+-- define sales funnel and the different stages 
+
+WITH funnel_stages AS (
+  SELECT
+    COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END) AS stage_1_views,
+    COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END) AS stage_2_cart,
+    COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' THEN user_id END) AS stage_3_checkout,
+    COUNT(DISTINCT CASE WHEN event_type = 'payment_info' THEN user_id END) AS stage_4_payment,
+    COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS stage_5_purchase
+
+  FROM `project-1a7f0a69-00af-4bc7-9b9.sql_practice.user_events`
+
+  WHERE event_date >= TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 300 DAY))
+)
+
+SELECT * FROM funnel_stages
